ef41a7e7f1208aff29f74f6f935f14dc087fa4d0
Littggr <littggr@gmail.com>
2014-01-06 13:49:43 -0500

    Exercises 1-3 complete

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#prints "I will now count my chickens"
+print "I will not count my chickens:"
+# prints "Hens" then prints the solution to 25+30/6
+print "Hens", 25 + 30 / 6
+#prints "Roosters" then prints the solution to 100 minus 25 times 3 modulus 4
+print "Roosters", 100 - 25 * 3 % 4
+#prints "Now i will count the eggs:"
+print "Now I will count the eggs:"
+# prints the solution to 3 plus 2 plus 1 minus 5 plus 4 modulus 2 minus 1 divided by 4 plus 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#prints "Is it true that 3 + 2 < 5 - 7?"
+print "Is it true that 3 + 2 < 5 - 7?"
+#prints the solution to 3 plus 2 less than 5 minus 7
+print 3 + 2 < 5 - 7
+# prints "What is 3 + 2?" then the solution to 3 plus 2
+print "What is 3 + 2?", 3 + 2
+# prints "What is 5 - 7?" then the solution to 5 minus 7
+print "What is 5 - 7?", 5 - 7
+#prints "Oh, that's why it's False."
+print "Oh, that's why it's False."
+#prints "How about some more."
+print "How about some more."
+#prints "Is it greater?" then the solution to 5 greater then negative 2
+print "Is it greater?", 5 > -2
+#prints "Is it greater or equal?" then the solution to 5 greater then or equal to  negative 2
+print "Is is greater or equal?", 5 >= -2
+#prints "Is it less or equal?" then the solution to 5 less than or equal to negative 2
+print "Is it less or equal?", 5 <= -2
